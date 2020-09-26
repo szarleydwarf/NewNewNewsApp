27efdf81d3816d309e2e839a702a0d77d8c4341c
RJ <r.j.chomik@gmail.com>
2020-09-26 21:47:04 +0100

    Category view ready

--- NewsNewsNews/ViewControllers/ViewController.swift
+++ NewsNewsNews/ViewControllers/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     let cellIdentifier = "theCell"
-    var url:URL?
-    let newsCategory = NewsCategoryAPI.shared
-    var newsArray:[NewsCategory]=[]
+    let urlString:String = "https://newsapi.org/v2/sources?apiKey=86316c5c482a49cca90420b39ee0a695"
+    var categoriesArray: [String] = []
     
     fileprivate func setTableView() {
         self.tableView.dataSource = self
@@ -21,19 +20,49 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
     }
     
+    fileprivate func fetchCategories() {
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {return}
+            let sources = try? JSONDecoder().decode(Sources.self, from: data)
+            guard let unwrapedSources = sources else {return}
+            for category in unwrapedSources.sources {
+                self.categoriesArray.append(category.category)
+            }
+            
+            DispatchQueue.main.async {
+                self.categoriesArray = Array(Set(self.categoriesArray))
+                self.tableView.reloadData()
+            }
+        }.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTableView()
-        newsArray = newsCategory.fetchData()
+        
+        fetchCategories()
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.categoriesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.text = self.categoriesArray[indexPath.row].capitalized
+        
+        return cell
     }
 }
 
+
+struct Sources: Decodable {
+    let sources:[NewsCategory]
+}
+
+struct NewsCategory:Decodable {
+    let category: String
+}
